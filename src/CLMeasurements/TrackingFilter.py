e5a3637ee43e3129afaee45b9c62bc9479373cfc
Jeff Davis <jeff@loudifier.com>
2025-03-25 02:16:20 -0700

    implement tracking filter measurement UI

--- src/CLMeasurements/TrackingFilter.py
+++ src/CLMeasurements/TrackingFilter.py
@@ -1,10 +1,11 @@
 import CLProject as clp
 from CLAnalysis import chirp_time_to_freq, chirp_freq_to_time, freq_points, interpolate, FS_to_unit
-from CLGui import CLParamNum, CLParamDropdown, FreqPointsParams
+from CLGui import CLParamNum, CLParamDropdown, FreqPointsParams, QCollapsible, QHSeparator
 import numpy as np
 from CLMeasurements import CLMeasurement
 from Biquad import Biquad, lowpass_coeff, highpass_coeff, bandpass_coeff, notch_coeff
 import pandas as pd
+from qtpy.QtWidgets import QFrame, QVBoxLayout, QAbstractSpinBox, QPushButton
 
 # tracking filter implementation to perform measurements roughly equivalent to Audio Precision's Rub and Buzz Peak Ratio and Crest Factor. https://www.ap.com/fileadmin-ap/technical-library/appnote-rub-buzz.pdf
 # for a Peak Ratio-style measurement, apply a highpass filter at 5-30x the fundamental and measure the 'filtered peak' signal relative to the 'fundamental RS' or 'unfiltered RMS' signal
@@ -23,6 +24,12 @@ class TrackingFilter(CLMeasurement):
                'filtered peak',   # peak level of the response signal filtered using the set of filters given in params['filters']. Peak is the max level in the interval of samples around each output frequency point
                'filtered RMS']    # RMS level of the filtered response signal
     
+    # set of filter types that can be selected
+    FILTER_TYPES = ['lowpass',  # 2nd order lowpass
+                    'highpass', # 2nd order highpass
+                    'bandpass', # constant peak (0dB at center frequency)
+                    'notch']    # notch with infinite depth (within limits of numerical precision and time/phase alignment)
+
     def __init__(self, name, params=None):
         if params is None:
             params = {}
@@ -32,11 +39,11 @@ class TrackingFilter(CLMeasurement):
         if len(params)<3: # default measurement parameters
             self.params['filters'] = [ # bank of 2nd order filters to apply to the response signal
                 {'multiplier': 10,     # the corner or center frequency of the filter ("wherever it's happenin', man") as a ratio of the instantaneous chirp frequency. e.g. for a multiplier of 10, when the chirp frequency is 1kHz the filter frequency will be 10kHz. Very high filter frequencies will be limited to 0.95*Nyquist
-                 'type': 'highpass',   # options are 'lowpass', 'highpass', 'bandpass' (constant peak=0dB at center frequency), and 'notch'
-                 'Q': 0.5},            # Q of the filter
+                 'type': 'highpass',   # options are 'lowpass', 'highpass', 'bandpass', and 'notch'
+                 'Q': 0.707},            # Q of the filter
                 {'multiplier': 10,
-                 'type': 'highpass',   # default filters results in a 4th order Butterworth highpass at 10x chirp frequency
-                 'Q': 0.5}]
+                 'type': 'highpass',   # default filters results in a 4th order highpass with Q=0.5 at 10x chirp frequency
+                 'Q': 0.707}]
                 # todo: add an option to define elliptical or higher order filters usng scipy.signal.iirdesign() or similar? Would make it easier for poeple to specify very sharp cutoffs, 8+ order Butterworth, etc. Actual filter processing would be pretty similar to biquad calculations by using 'sos' conversion
             self.params['mode'] = 'relative' # output the filtered response amplitude 'relative' to a reference signal or 'absolute'
             self.params['measured_signal'] = 'filtered peak' # 'filtered peak' relative to 'fundamental' is roughly equivalent to AP's Rub and Buzz Peak Ratio measurement
@@ -206,16 +213,131 @@ class TrackingFilter(CLMeasurement):
     def init_tab(self):
         super().init_tab()
 
-        # measurement parameters
+        # dropdown to select measured signal
+        self.measured_signal = CLParamDropdown('Measured signal', self.SIGNALS)
+        signal_index = self.measured_signal.dropdown.findText(self.params['measured_signal'])
+        self.measured_signal.dropdown.setCurrentIndex(signal_index)
+        self.param_section.addWidget(self.measured_signal)
+        def update_measured_signal(index):
+            self.params['measured_signal'] = self.SIGNALS[index]
+            self.measure()
+            self.plot()
+        self.measured_signal.update_callback = update_measured_signal
 
-        
-        self.output_unit = CLParamDropdown('Units', [unit for unit in self.RELATIVE_OUTPUT_UNITS], '')
+        # dropdown to select relative or absolute mode
+        self.mode = CLParamDropdown('Measurement mode', ['relative', 'absolute'])
+        if self.params['mode'] == 'absolute':
+            self.mode.dropdown.setCurrentIndex(1)
+        self.param_section.addWidget(self.mode)
+        def update_mode(index):
+            self.output_unit.dropdown.blockSignals(True)
+            self.output_unit.dropdown.clear()
+            if index:
+                self.params['mode'] = 'absolute'
+                self.reference_signal.dropdown.setEnabled(False)
+                self.output_unit.dropdown.addItems(self.ABSOLUTE_OUTPUT_UNITS)
+            else:
+                self.params['mode'] = 'relative'
+                self.reference_signal.dropdown.setEnabled(True)
+                self.output_unit.dropdown.addItems(self.RELATIVE_OUTPUT_UNITS)
+            self.output_unit.dropdown.blockSignals(False)
+            update_output_unit(0) # sets relative to 'dB', sets absolute to 'dBFS'
+        self.mode.update_callback = update_mode
+
+        # dropdown to set reference signal for relative mode
+        self.reference_signal = CLParamDropdown('Reference signal', self.SIGNALS)
+        signal_index = self.reference_signal.dropdown.findText(self.params['reference_signal'])
+        self.reference_signal.dropdown.setCurrentIndex(signal_index)
+        self.param_section.addWidget(self.reference_signal)
+        def update_reference_signal(index):
+            self.params['reference_signal'] = self.SIGNALS[index]
+            self.measure()
+            self.plot()
+        self.reference_signal.update_callback = update_reference_signal
+
+        # RMS averaging time
+        self.rms_time = CLParamNum('RMS time', self.params['rms_time'], ['seconds', 'octaves'])
+        self.rms_time.spin_box.setDecimals(3)
+        if self.params['rms_unit'] == 'octaves':
+            self.rms_time.min = 1/48
+            self.rms_time.units.setCurrentIndex(1)
+        else:
+            self.rms_time.min = 0.001
+        self.param_section.addWidget(self.rms_time)
+        def update_rms_time(new_val):
+            self.params['rms_time'] = new_val
+            self.measure()
+            self.plot()
+        self.rms_time.update_callback = update_rms_time
+        def update_rms_time_unit(index):
+            sweep_rate = clp.project['chirp_length'] / np.log2(clp.project['stop_freq'] / clp.project['start_freq'])
+            if index:
+                self.params['rms_unit'] = 'octaves'
+                self.params['rms_time'] /= sweep_rate
+            else:
+                self.params['rms_unit'] = 'seconds'
+                self.params['rms_time'] *= sweep_rate
+            self.rms_time.set_value(self.params['rms_time'])
+        self.rms_time.units_update_callback = update_rms_time_unit
+
+        # collapsible section with controls for filter bank
+        self.filters_section = QCollapsible('Filters')
+        self.param_section.addWidget(self.filters_section)
+
+        # list of clump of controls to configure each filter
+        self.filters_params = [None]*len(self.params['filters'])
+        for f in range(len(self.params['filters'])):
+            self.filters_params[f] = FilterParams(self.params['filters'][f], self)
+            self.filters_section.addWidget(self.filters_params[f])
+
+        # button to remove the last filter from the list
+        self.remove_filter_button = QPushButton('Remove filter')
+        def remove_filter():
+                self.filters_section.removeWidget(self.filters_params[-1])
+                self.filters_params.pop()
+                self.params['filters'].pop()
+                if len(self.params['filters']) == 1: # always leave at least one filter
+                    self.filters_section.removeWidget(self.remove_filter_button)
+                self.measure()
+                self.plot()
+        self.remove_filter_button.clicked.connect(remove_filter)
+        if len(self.params['filters']) > 1:
+            self.filters_section.addWidget(self.remove_filter_button)
+            
+        # button (and horizontal separator) to add another filter to the filter bank
+        self.add_button_separator = QHSeparator()
+        self.filters_section.addWidget(self.add_button_separator)
+
+        self.add_filter_button = QPushButton('Add filter')
+        self.filters_section.addWidget(self.add_filter_button)
+        def add_filter():
+            self.params['filters'].append({'type': 'highpass',
+                                          'multiplier': 10,
+                                          'Q': 0.707})
+            self.filters_params.append(FilterParams(self.params['filters'][-1], self))
+            self.filters_section.removeWidget(self.add_filter_button)
+            self.filters_section.removeWidget(self.add_button_separator)
+            self.filters_section.removeWidget(self.remove_filter_button)
+            self.filters_section.addWidget(self.filters_params[-1])
+            self.filters_section.addWidget(self.remove_filter_button)
+            self.filters_section.addWidget(self.add_button_separator)
+            self.filters_section.addWidget(self.add_filter_button)
+            self.measure()
+            self.plot()
+        self.add_filter_button.clicked.connect(add_filter)
+
+
+        # output parameters
+        if self.params['mode'] == 'absolute':
+            self.output_unit = CLParamDropdown('Units', self.ABSOLUTE_OUTPUT_UNITS, '')
+        else:
+            self.output_unit = CLParamDropdown('Units', self.RELATIVE_OUTPUT_UNITS, '')
         output_unit_index = self.output_unit.dropdown.findText(self.params['output']['unit'])
         if output_unit_index != -1:
             self.output_unit.dropdown.setCurrentIndex(output_unit_index)
         self.output_section.addWidget(self.output_unit)
         def update_output_unit(index):
-            self.params['output']['unit'] = self.OUTPUT_UNITS[index]
+            self.params['output']['unit'] = self.output_unit.dropdown.currentText()
             self.measure()
             self.plot()
             self.format_graph()
@@ -239,5 +361,42 @@ class TrackingFilter(CLMeasurement):
         return clp.project['start_freq']
     
     def calc_auto_max_freq(self):
-        # todo: make this function smarter. Does it make sense to just divide stop_freq by max filter frequency multiplier
+        # todo: make this function smarter. Does it make sense to just divide stop_freq by max filter frequency multiplier?
         return clp.project['stop_freq']
+
+class FilterParams(QFrame):
+    # pass in the parameters dict for the filter so they can be updated, and the parent TrackingFilter to make it easy to call measure() and plot()
+    def __init__(self, params, measurement):
+        super().__init__()
+
+        layout = QVBoxLayout(self)
+
+        layout.addWidget(QHSeparator())
+
+        self.type = CLParamDropdown('Filter type', measurement.FILTER_TYPES)
+        type_index = self.type.dropdown.findText(params['type'])
+        self.type.dropdown.setCurrentIndex(type_index)
+        layout.addWidget(self.type)
+        def update_type(index):
+            params['type'] = measurement.FILTER_TYPES[index]
+            measurement.measure()
+            measurement.plot()
+        self.type.update_callback = update_type
+        
+        self.multiplier = CLParamNum('Frequency', params['multiplier'], 'x chirp fundamental', 0.01)
+        self.multiplier.spin_box.setStepType(QAbstractSpinBox.StepType.DefaultStepType)
+        layout.addWidget(self.multiplier)
+        def update_multiplier(new_val):
+            params['multiplier'] = new_val
+            measurement.measure()
+            measurement.plot()
+        self.multiplier.update_callback = update_multiplier
+
+        self.Q = CLParamNum('Q', params['Q'], '', 0.1)
+        self.Q.spin_box.setDecimals(3)
+        layout.addWidget(self.Q)
+        def update_Q(new_val):
+            params['Q'] = new_val
+            measurement.measure()
+            measurement.plot()
+        self.Q.update_callback = update_Q
