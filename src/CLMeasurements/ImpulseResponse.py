f106cfda231fb22c48534e010ca000b5d857e913
Jeff Davis <jeff@loudifier.com>
2025-04-10 23:34:28 -0700

    implement everything except for output UI elements and saving data as a wav file

--- src/CLMeasurements/ImpulseResponse.py
+++ src/CLMeasurements/ImpulseResponse.py
@@ -2,10 +2,12 @@ import CLProject as clp
 from CLMeasurements import CLMeasurement
 from scipy.fftpack import fft, ifft
 from scipy.signal.windows import hann
-from CLGui.CLParameter import CLParamDropdown
+from CLGui.CLParameter import CLParamDropdown, CLParamNum
 from CLMeasurements.FrequencyResponse import WindowParamsSection, ms_to_samples, samples_to_ms
 import numpy as np
 import pyqtgraph as pg
+from qtpy.QtWidgets import QCheckBox
+from qtpy.QtCore import Qt
 
 class ImpulseResponse(CLMeasurement):
     measurement_type_name = 'Impulse Response'
@@ -15,6 +17,8 @@ class ImpulseResponse(CLMeasurement):
     WINDOW_MODES = ['raw', 'windowed', 'auto']
     MAX_WINDOW_START = 1000 # fixed impulse response window can start up to 1s before t0
     MAX_WINDOW_END = 10000 # IR window can end up to 10s after t0
+
+    ALIGNMENT_MODES = ['window start', 't=0', 'centered', 'fixed offset']
 
     def __init__(self, name, params=None):
         if params is None:
@@ -94,13 +98,17 @@ class ImpulseResponse(CLMeasurement):
         # store measurement data
         self.out_ir = impulse_response
 
+        # generate sample timestamps for graphs
+        self.out_times = samples_to_ms(np.arange(len(impulse_response)) - roll_samples)
+
         # calculate noise IR
         if any(clp.signals['noise']):
             noise_ir = ifft(fft(clp.signals['noise']) / fft(clp.signals['stimulus'])).real
-            noise_ir *= window
+            if self.params['window_mode'] != 'raw':
+                noise_ir *= window
             self.out_noise = np.roll(noise_ir, roll_samples)
 
-        if self.params['plot_window']:
+        if self.params['plot_window'] and self.params['window_mode']!='raw':
             # apply offset and scale window appropriately for plotting
             self.out_window = np.roll(window, roll_samples) * max(abs(self.out_ir))
         
@@ -122,6 +130,9 @@ class ImpulseResponse(CLMeasurement):
             else:
                 self.window_params.collapse(animate=self.window_params.isExpanded())
                 self.window_params.setLocked(True)
+            if self.params['window_mode']=='raw' and self.params['alignment']=='window_start':
+                self.alignment.dropdown.setCurrentIndex(3) # change alignment to 'offset'
+                return # let update_alignment call measure() and plot()
             self.measure()
             self.plot()
         self.window_mode.update_callback = update_window_mode
@@ -137,9 +148,63 @@ class ImpulseResponse(CLMeasurement):
         self.window_params.update_callback = update_window_params
 
         # alignment dropdown
-        # todo: make sure to change alignment from 'window_start' to 'offset' if window_mode is changed to 'raw'
+        self.alignment = CLParamDropdown('Time alignment', self.ALIGNMENT_MODES)
+        match self.params['alignment']:
+            case 'window_start':
+                alignment_index = 0
+            case 't=0':
+                alignment_index = 1
+            case 'centered':
+                alignment_index = 2
+            case 'offset':
+                alignment_index = 3
+        self.alignment.dropdown.setCurrentIndex(alignment_index)
+        self.param_section.addWidget(self.alignment)
+        def update_alignment(index):
+            match index:
+                case 0:
+                    self.params['alignment'] = 'window_start'
+                case 1:
+                    self.params['alignment'] = 't=0'
+                case 2:
+                    self.params['alignment'] = 'centered'
+                case 3:
+                    self.params['alignment'] = 'offset'
+                    self.offset.setEnabled(True)
+            if index < 3:
+                self.offset.setEnabled(False)
+            self.measure()
+            self.plot()
+        self.alignment.update_callback = update_alignment
+
+        # fixed offset spinbox
+        self.offset = CLParamNum('Offset', self.params['offset'], ['ms', 'samples'], 0, samples_to_ms(len(clp.signals['stimulus'])))
+        self.offset.setEnabled(self.params['alignment']=='offset')
+        self.param_section.addWidget(self.offset)
+        def update_offset(new_val):
+            if self.offset.units.currentIndex(): # samples
+                self.params['offset'] = samples_to_ms(new_val)
+            else: # ms
+                self.params['offset'] = new_val
+            self.measure()
+            self.plot()
+        self.offset.update_callback = update_offset
+        def update_offset_unit(index):
+            if index: # changing from ms to samples
+                self.offset.max = len(clp.signals['stimulus'])
+                self.offset.set_value(ms_to_samples(self.params['offset']))
+            else: # changing from samples to ms
+                self.offset.set_value(self.params['offset'])
+                self.offset.max = samples_to_ms(len(clp.signals['stimulus']))
+        self.offset.units_update_callback = update_offset_unit
+        self.update_offset_unit = update_offset_unit
+
         
         # plot window checkbox
+        self.plot_window = QCheckBox('Plot window')
+        self.plot_window.setChecked(True)
+        self.param_section.addWidget(self.plot_window)
+        self.plot_window.stateChanged.connect(self.plot)
 
 
         # output parameters
@@ -151,8 +216,9 @@ class ImpulseResponse(CLMeasurement):
 
     def update_tab(self):
         self.window_params.update_window_params()
+        self.update_offset_unit(self.offset.units.currentIndex())
 
-    # time series measurement output requires several base CLMeasurement methods to be overridden
+    # time series measurement output requires some base CLMeasurement methods to be overridden
     def save_measurement_data(self, out_path=''):
         pass
     
@@ -165,11 +231,34 @@ class ImpulseResponse(CLMeasurement):
         self.tab.graph.clear()
         
         plot_pen = pg.mkPen(color=clp.PLOT_COLORS[0], width=clp.PLOT_PEN_WIDTH)
-        self.tab.graph.plot(self.out_ir, name = self.measurement_type_name, pen=plot_pen) # todo: do something different when plotting a single point (pyqtgraph falls on its face otherwise)
+        self.tab.graph.plot(self.out_times, self.out_ir, name = self.measurement_type_name, pen=plot_pen) # todo: do something different when plotting a single point (pyqtgraph falls on its face otherwise)
         
         if any(self.out_noise):
             noise_pen = pg.mkPen(color=clp.NOISE_COLOR, width=clp.PLOT_PEN_WIDTH)
-            self.tab.graph.plot(self.out_noise, name='Noise Floor', pen=noise_pen)#, color='gray')
+            self.tab.graph.plot(self.out_times, self.out_noise, name='Noise Floor', pen=noise_pen)#, color='gray')
+
+        # set the plot range based on the active portion of the impulse response
+        if self.params['window_mode'] == 'raw':
+            self.tab.graph.setXRange(self.out_times[0], self.out_times[-1])
+            return # don't plot the window in raw mode
+        if self.params['window_mode'] == 'auto':
+            max_wavelength_ms = 1000 * 1 / clp.project['start_freq']
+            graph_min = max(-max_wavelength_ms * 1.1, self.out_times[0])
+            graph_max = max_wavelength_ms * 2.1
+        else: # 'windowed' case
+            graph_min = max(-self.params['window_start'] * 1.1, self.out_times[0])
+            graph_max = self.params['window_end'] * 1.1
+        
+        # plot the window
+        if self.plot_window.isChecked():
+            window_pen = pg.mkPen(color=clp.PLOT_COLORS[1], width=clp.PLOT_PEN_WIDTH, style=Qt.DotLine)
+            self.tab.graph.plot(self.out_times, self.out_window, name='Window', pen=window_pen)
+        
+        # plot the full range if the window wraps around the beginning/end of the impulse response
+        if self.out_window[-1] > 0:
+            self.tab.graph.setXRange(self.out_times[0], self.out_times[-1])
+        else:
+            self.tab.graph.setXRange(graph_min, graph_max) # todo: figure out why plot range is actually lower than graph_min sometimes
 
 
         
