1d9a95bbeb5e68413bdb112a81a616125b8f7853
Jeff Davis <jeff@loudifier.com>
2026-04-25 15:22:30 -0700

    keep mousewheel from scrolling widgets in panel areas

--- src/CLGui/CLTab.py
+++ src/CLGui/CLTab.py
@@ -1,4 +1,4 @@
-from qtpy.QtWidgets import QSplitter, QVBoxLayout, QScrollArea, QFrame, QWidget
+from qtpy.QtWidgets import QSplitter, QVBoxLayout, QScrollArea, QFrame, QWidget, QApplication
 from qtpy import QtCore
 import pyqtgraph as pg
 from CLGui import EngAxisItem
@@ -16,6 +16,11 @@ class CLTab(QSplitter): # base widget is a splitter
         panel_frame.setLayout(self.panel)
         panel_scroll.setWidget(panel_frame)
         self.panel.setAlignment(QtCore.Qt.AlignTop)
+
+        # catch all mouse scrolls in panel area and don't pass on to children
+        # allows you to scroll without accidentally changing dropdowns, spinbox values, etc.
+        self.scroll_handler = ScrollHandler(panel_frame) # panel_frame is the object that actually handles the scroll events
+        QApplication.instance().installEventFilter(self.scroll_handler) # could install filters on all child objects, but filtering the whole application seems to be the only way to reliably catch all scroll events
                 
         # graph area setup
         self.graph = pg.PlotWidget(axisItems={'bottom':EngAxisItem('bottom')})
@@ -33,3 +38,24 @@ class CLTab(QSplitter): # base widget is a splitter
         # set initial panel width
         panel_width = 235 # todo: reasonable initial value on my machine. Panel size/scaling will need a lot of work #DPI
         self.setSizes([panel_width, self.window().width()-panel_width])
+
+
+class ScrollHandler(QtCore.QObject):
+    def __init__(self, scroll_panel):
+        super().__init__()
+        self.scroll_panel = scroll_panel
+
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.Type.Wheel: # some other events should probably also be caught. Still activates widgets when you scroll past them
+            if self.is_in_scroll_panel(obj):
+                QtCore.QCoreApplication.sendEvent(self.scroll_panel, event)
+                return True
+        return super().eventFilter(obj, event)
+    
+    def is_in_scroll_panel(self, obj):
+        parent = obj.parent()
+        while parent is not None:
+            if parent is self.scroll_panel:
+                return True
+            parent = parent.parent()
+        return False
